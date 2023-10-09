fba8c6f2edcbe8a4c49b2c2df7aeee21217f1582
SUDIP MAHATO <mahatosudip837@gmail.com>
2023-10-09 19:56:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// navbar toggle 
+let navBar = document.querySelector('.navLink');
+let menuBar = document.querySelector('#menuBtn');
+
+menuBar.onclick = ()=>{
+    navBar.classList.toggle('active')
+}
+
+// scroll section & sticky navbar 
+window.onscroll = ()=>{
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+}
+
+// swiper js code 
+let swiper = new Swiper(".myHome", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
